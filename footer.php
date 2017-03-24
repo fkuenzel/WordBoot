a6d5b959a5cd523f00cd13562850b46c51c061c7
Fabian Künzel <fabian.kuenzel@gmail.com>
2017-03-24 08:09:59 +0100

    Link zu WordBoot geändert.

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 			
 				<div class="<?php wb_container_class(); ?>">
 					<div class="copyright">
-						<p>Copyright <?php echo date('Y'); ?> by <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>. | Make with <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wordboot' ) ); ?>"><?php echo  __( 'WordPress', 'wordboot' ); ?></a> and <a href="<?php echo esc_url( __( 'https://fkuenzel.de/theme/wordboot/', 'wordboot' ) ); ?>"><?php echo __( "WordBoot ", "wordboot" ) .' '. $GLOBALS['wordboot_version']; ?></a><p>
+						<p>Copyright <?php echo date('Y'); ?> by <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>. | Make with <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wordboot' ) ); ?>"><?php echo  __( 'WordPress', 'wordboot' ); ?></a> and <a href="<?php echo esc_url( __( 'https://github.com/fkuenzel/WordBoot', 'wordboot' ) ); ?>"><?php echo __( "WordBoot ", "wordboot" ) .' '. $GLOBALS['wordboot_version']; ?></a><p>
 					</div>
 				</div>
 			</footer>
