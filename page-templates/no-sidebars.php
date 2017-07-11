efed52fe28a71303d5e3ce45bf9388f8e794ba83
Fabian Künzel <fabian.kuenzel@gmail.com>
2017-07-11 21:47:51 +0200

    Page Template Ohne Sidebars
    Hinzufügen eines Seiten-Templates für volle Container Nutzung (Ohne
    Sidebars)

---
+++ page-templates/no-sidebars.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying all PAGES posts with NO sidebar
+ *
+ * Template Name: Page NO Sidebars
+ *
+ * @package WordPress
+ * @subpackage bs4_basis
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+	<div class="<?php bs4_container_class(); ?>" id="pageContent">
+		
+		
+	
+		<div class="row">
+			<div class="<?php layout_grid_class( 'content' ); ?>">
+			<?php
+				while ( have_posts() ) : the_post();
+
+					get_template_part( 'template-parts/page/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() AND bs4_show_comments_on_pages() == TRUE ) {
+						comments_template();
+					} else {
+						bs4_comments_alert();
+					}
+
+				endwhile; // End of the loop.
+			?>
+			</div> <!-- .col -->
+						
+		</div> <!-- .row end; -->
+	</div> <!-- .<?php bs4_container_class(); ?> AUTO CLASS end -->
+<?php get_footer(); ?>
