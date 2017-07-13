93f695b3403bb662708084c971898da5d116cafb
Fabian <fabian.kuenzel@gmail.com>
2017-07-13 08:50:41 +0200

    changing copyright info
    read theme name and URI for Copyright from Theme Stylesheets

--- footer.php
+++ footer.php
@@ -13,8 +13,11 @@
 			
 			
 			
-				<div class="copyright text-center text-muted">
-					<p>Copyright <?php echo date('Y'); ?> by <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>"><?php bloginfo( 'name' ); ?></a>. | Make with <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bs4_lang' ) ); ?>" title="<?php echo  __( 'WordPress', 'bs4_lang' ); ?>" rel="external"><?php echo  __( 'WordPress', 'bs4_lang' ); ?></a> and <a href="<?php echo esc_url( __( 'https://github.com/fkuenzel/WordBoot', 'bs4_lang' ) ); ?>" title="<?php echo __( "WordBoot ", "bs4_lang" ) .' '. $GLOBALS['bs4_version']; ?>" rel="external"><?php echo __( "WordBoot ", "bs4_lang" ) .' '. $GLOBALS['bs4_version']; ?></a></p>
+				<?php
+					$theme_data = wp_get_theme(); 
+				?>
+				<div class="copyright text-center">
+					<p>Copyright <?php echo date('Y'); ?> by <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>"><?php bloginfo( 'name' ); ?></a>. | Make with <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bs4_lang' ) ); ?>" title="<?php echo  __( 'WordPress', 'bs4_lang' ); ?>" rel="external"><?php echo  __( 'WordPress', 'bs4_lang' ); ?></a> and <a href="<?php echo esc_url( $theme_data->get('ThemeURI') ); ?>" title="<?php echo esc_html($theme_data->get('Name')) .' '. esc_html($theme_data->get('Version')); ?>" rel="external"><?php echo esc_html($theme_data->get('Name')) .' '. esc_html($theme_data->get('Version')); ?></a></p>
 				</div>
 
 			</footer>
